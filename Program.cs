1659c5fecd71eec62aef36ddcee586663610dfc7
Tomas Kindahl <tomas.kindahl@molk.se>
2020-11-03 09:33:33 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopNoteDeLuxe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
